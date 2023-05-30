a24cac230a7d26cc8b70be2dfca07f77196942e9
santoshnaruje <santoshnaruje@gmail.com>
2023-05-30 16:06:46 +0530

    added employee form

--- src/Components/AllEmployees/index.js
+++ src/Components/AllEmployees/index.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import {Vortex} from 'react-loader-spinner';
+import { BsX } from "react-icons/bs";
 import IndividualEmployee from '../IndividualEmployee';
 import './index.css'
 
@@ -7,12 +8,116 @@ import './index.css'
 const AllEmployees = () => {
   const [employees, setEmployees] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [employeeData,setEmployeeData]=useState({first_name:"",id:"",
+  date_of_birth:"",
+  qualifications:"",
+  department:"",
+  salary:"",
+  username:"",
+  last_name:"",
+  email:"",
+  address:"",
+  designation:"",
+  joining_date:"",
+})
+
+ const handleAddEmployeeForm=(e)=>{
+  e.preventDefault()
+  
+
+ }
+ const OnchnageHandler=(e)=>{
+  console.log([e.target.name])
+  setEmployeeData({
+    ...employeeData,
+    [e.target.name]: e.target.value
+  }
+)
+
+ }
   useEffect(() => {
     fetchEmployees();
 
   }, [])
 
+  const openPopup = () => {
+    setIsPopupOpen(true);
+  };
 
+  const closePopup = () => {
+    setIsPopupOpen(false);
+  };
+
+  const popupForm = () => (
+    <form className='form-group' onSubmit={handleAddEmployeeForm}>
+      <div className="d-flex justify-content-end m-0">
+        <button className="close-button" onClick={closePopup}>
+          <BsX />
+        </button>
+      </div>
+  
+      <div className="d-flex">
+        <div className="left-container">
+          <div className="form-field">
+            <label htmlFor="firstName">First Name</label>
+            <input type="text" id='firstName' name='first_name' onChange={OnchnageHandler} />
+          </div>
+          <div className="form-field">
+            <label htmlFor="lastName">Last Name</label>
+            <input type="text" className="input" name='last_name' onChange={OnchnageHandler} />
+          </div>
+          <div className="form-field">
+            <label htmlFor="id">ID</label>
+            <input type="text" className="input" name='id' onChange={OnchnageHandler} />
+          </div>
+          <div className="form-field">
+            <label htmlFor="email">Email</label>
+            <input type="text" className="input" name='email' onChange={OnchnageHandler} />
+          </div>
+          <div className="form-field">
+            <label htmlFor="dob">Date of Birth</label>
+            <input type="date" className="input" name='dob' onChange={OnchnageHandler} />
+          </div>
+          <div className="form-field">
+            <label htmlFor="address">Address</label>
+            <input type="text" className="input" name='address' onChange={OnchnageHandler} />
+          </div>
+        </div>
+        <div>
+          <div className="form-field">
+            <label htmlFor="username">Username</label>
+            <input type="text" className="input" name='username' onChange={OnchnageHandler} />
+          </div>
+          <div className="form-field">
+            <label htmlFor="qualifications">Qualifications</label>
+            <input type="text" className="input" name='qualifications' onChange={OnchnageHandler} />
+          </div>
+          <div className="form-field">
+            <label htmlFor="joiningDate">Joining Date</label>
+            <input type="date" className="input" name='joining_date' onChange={OnchnageHandler} />
+          </div>
+          <div className="form-field">
+            <label htmlFor="designation">Designation</label>
+            <input type="text" className="input" name='designation' onChange={OnchnageHandler} />
+          </div>
+          <div className="form-field">
+            <label htmlFor="salary">Salary</label>
+            <input type="text" className="input" name='salary' onChange={OnchnageHandler} />
+          </div>
+          <div className="form-field">
+            <label htmlFor="department">Department</label>
+            <input type="text" className="input" name='department' onChange={OnchnageHandler} />
+          </div>
+        </div>
+      </div>
+  
+      <button type="submit" className="mt-3 submit-btn">
+        Submit
+      </button>
+    </form>
+  );
+  
 
   const fetchEmployees = async () => {
     try {
@@ -46,6 +151,9 @@ const AllEmployees = () => {
   return (
     <div className='d-flex flex-column  align-items-center mt-2 '>
       <h1 className='heading'>All Employees of Openskale</h1>
+      <button className="add-employee-btn" onClick={openPopup}>
+        Add Employee
+      </button>
       {isLoading ? (
         <div className=' tailspin d-flex flex-column justify-content-center'>
           <Vortex
@@ -86,7 +194,15 @@ const AllEmployees = () => {
 </tbody>
         </table>
       )}
+      <div>
+        {isPopupOpen && (
+          <div className="popup-overlay">
+            <div className="popup-content">{popupForm()}</div>
+          </div>
+        )}
+      </div>
     </div>
+
   );
 };
 
